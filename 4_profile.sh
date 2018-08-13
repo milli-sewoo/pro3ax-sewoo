33754c39e1783449f427dabb35cd740605a9b696
milli-sewoo <42341308+milli-sewoo@users.noreply.github.com>
2018-08-13 21:23:13 +0900

    Update 4_profile.sh

--- 4_profile.sh
+++ 4_profile.sh
@@ -13,9 +13,9 @@ sleep 20
 
 # 히스토리 사이즈 변경 (1000개 -> 100,000개)
 
-echoecho  $HISTSIZE$HISTSIZE
+echo $HISTSIZE
 grep HISTSIZE /etc/skel/.bashrc
-perl -pi -e  grep HISTSIZE /etc/skel/.bashrc  's/HISTSIZE=1000/HISTSIZE=100000/'   /etc/skel/.bashrc
+perl -pi -e 's/HISTSIZE=1000/HISTSIZE=100000/'   /etc/skel/.bashrc
 grep HISTSIZE /etc/skel/.bashrc
 
 grep HISTSIZE .bashrc
