428bf16a47c2cbe430e29db1ca2b291ee333133a
milli-sewoo <42341308+milli-sewoo@users.noreply.github.com>
2018-08-13 21:33:39 +0900

    Update and rename 4_profile.sh to 4-1profile.sh

---
+++ 4-1profile.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# 기본 alias
+
+echo " "  >> /etc/profile
+echo "# add by dasandata"             >>   /etc/profile
+echo "alias vi='vim' "                        >>   /etc/profile
+echo "alias ls='ls --color=auto' "       >>   /etc/profile
+echo "alias ll='ls -lh' "                        >>   /etc/profile
+echo "alias grep='grep --color=auto' "   >>   /etc/profile
