ed57253a2031c6b44561be12a674adeef502d805
milli-sewoo <42341308+milli-sewoo@users.noreply.github.com>
2018-08-14 11:26:15 +0900

    Update 1_repository.sh

--- 1_repository.sh
+++ 1_repository.sh
@@ -1,13 +1,13 @@
-#bin/bash
+#!bin/bash
 
-# 기본 저장소 확인
+echo " # 기본 저장소 확인 "
 cat /etc/apt/sources.list  |  grep -v "#\|^$"
 
-# Security 부분 변경
+echo " # Security 부분 변경 "
 perl -pi -e 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 
-# 변경 내용 확인
+echo " # 변경 내용 확인 "
 cat /etc/apt/sources.list  |  grep -v "#\|^$"
 
-# 적용 하기
+echo " # 적용 하기 "
 apt-get update
