4f916f311de72bcb004e65555446b16435014e08
milli-sewoo <42341308+milli-sewoo@users.noreply.github.com>
2018-08-14 11:38:47 +0900

    Update 3_util_dell.sh

--- 3_util_dell.sh
+++ 3_util_dell.sh
@@ -1,18 +1,34 @@
 #/bin/bash
 
-# 기본 유틸 설치.
+echo " # 기본 유틸 설치. "
 
 apt-get install -y  \
 vim nfs-common rdate xauth firefox gcc make locate htop tmux git wget ipmitool \
 xfsprogs ntfs-3g aptitude lvm2 dstat curl 
 
-sleep 20
+COUNTER=20
+
+until [  $COUNTER -lt 10 ]; do
+
+      echo COUNTER $COUNTER
+
+      let COUNTER-=1
+
+done
 
 aptitude install -y python-dev 
 
-sleep 10
+COUNTER=10
 
-# 서버 시간 동기화.
+until [  $COUNTER -lt 10 ]; do
+
+      echo COUNTER $COUNTER
+
+      let COUNTER-=1
+
+done
+
+echo " # 서버 시간 동기화. "
 
 rdate  -s  time.bora.net
 hwclock --systohc  
