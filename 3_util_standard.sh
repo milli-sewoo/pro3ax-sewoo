d6a5cb10f323f08500230bd91e4438662e7af647
milli-sewoo <42341308+milli-sewoo@users.noreply.github.com>
2018-08-14 11:44:26 +0900

    Update 3_util_standard.sh

--- 3_util_standard.sh
+++ 3_util_standard.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-# 기본 유틸 설치
+echo " # 기본 유틸 설치 "
 
 apt-get install -y  \
 vim nfs-common rdate xauth firefox gcc make locate htop tmux git wget \
@@ -8,11 +8,13 @@ xfsprogs ntfs-3g aptitude lvm2 dstat curl
 
 sleep 20
 
+echo " 파이선 설치 "
+
 aptitude install -y python-dev
 
 sleep 10 
 
-# 서버 시간 동기화.
+echo " # 서버 시간 동기화. " 
 rdate  -s  time.bora.net
 hwclock --systohc  
 date
